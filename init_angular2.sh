beeace6a6ccdfdc1b52f14c449d59613a25ba7c5
Renaud LITTOLFF <rlittolff@gmail.com>
2017-03-12 12:08:34 +0100

    Fix ng path

--- init_angular2.sh
+++ init_angular2.sh
@@ -3,4 +3,4 @@
 /init_nodejs.sh
 
 # sudo is needed to launch on port 80
-sudo /data/node_modules/bin/ng serve --host 0.0.0.0 --port 80 --live-reload-port 49094
+sudo /data/node_modules/.bin/ng serve --host 0.0.0.0 --port 80 --live-reload-port 49094
